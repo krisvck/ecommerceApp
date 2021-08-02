06f6921f3d0a257f795a6f2faedba4cb5c29c0e6
Krist Veruari <kveruari@gmail.com>
2021-08-02 16:44:08 -0600

    React

---
+++ frontend/src/App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+//import './index.css';
+import data from './data'
+
+function App() {
+
+  const openMenu = () => {
+    document.querySelector('.sidebar').classList.add('open');
+  };
+  const closeMenu = () => {
+    document.querySelector('.sidebar').classList.remove('open');
+  };
+
+  
+  return (
+    <BrowserRouter>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <button onClick={openMenu}>
+            &#9776;
+          </button>
+             <a href="index.html">K Store</a>
+        </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+      </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+              <ul>
+                <li>
+                  <a href="index.html">Pants</a>
+                </li>
+                <li>
+                  <a href="index.html">Shirts</a>
+                </li>
+              </ul>
+        </aside>
+        
+        <main className="main">
+          <div className="content">
+          <Route path="/products/:id" component={ProductScreen}/>
+          <Route path="/" exact={true} component={HomeScreen} />
+            <ul className="products">
+            {
+              data.products.map(product => 
+              <li>
+                <div className="product">
+                  <img className="product-image" src={product.image} alt="product"/>
+                    <div className="product-name">
+                      <a href="product.html">{product.name}</a>
+                    </div>
+                      <div className="product-brand">{product.brand}</div>
+                      <div className="product-price">{product.price}</div>
+                      <div className="product-rating">{product.rating} Stars {product.numReviews}</div>
+                </div>
+
+              </li>)
+            }
+              
+            </ul>
+          </div>
+            
+        </main>
+          <footer className="footer">All rights Reserved</footer>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,38 @@
+const data = {
+    products: [
+        {
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpeg',
+        price: 60,
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpeg',
+        price: 35,
+        rating: 4.1,
+        numReviews: 5
+    },
+    {
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '/images/d1.jpeg',
+        price: 70,
+        rating: 4.8,
+        numReviews: 3
+    },
+    {
+        name: 'Long Pants',
+        category: 'Pants',
+        image: '/images/d1.jpeg',
+        price: 56,
+        rating: 3.8,
+        numReviews: 7
+    },
+]
+}
+
+export default data;
